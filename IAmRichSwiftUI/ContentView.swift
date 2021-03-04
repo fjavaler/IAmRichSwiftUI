8326f297f37126ff828678a2a9226740e099a415
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-04 15:48:39 -0800

    Completed.

--- IAmRichSwiftUI/ContentView.swift
+++ IAmRichSwiftUI/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich").fontWeight(.bold)
+                .foregroundColor(Color.white)
+                    .padding().font(.system(size: 40))
+                Image("diamond").resizable().aspectRatio(contentMode: .fit).frame(width: 200.0, height: 200.0, alignment: .center)
+            }
+        }
     }
 }
 
