2974d0f91d362dfef88ef145983b866dd30f9f12
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-04 15:28:01 -0800

    Initial Commit

---
+++ IAmRichSwiftUI/IAmRichSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichSwiftUIApp.swift
+//  IAmRichSwiftUI
+//
+//  Created by Frederick Javalera on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
